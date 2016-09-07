8fdfe3989b0cc65902688363c98e8ebf06af7740
zijietang <Jason@Jasons-MacBook-Air.local>
2016-09-07 08:49:15 +0800

    导入github

---
+++ Application/Install/Data/update.sql
@@ -0,0 +1,17 @@
+ALTER TABLE `ujn_category` ADD COLUMN `groups` varchar(255) NOT NULL DEFAULT '' COMMENT '分组定义';
+ALTER TABLE `ujn_document` ADD COLUMN `group_id` smallint(3) unsigned NOT NULL COMMENT '所属分组' AFTER `category_id`;
+ALTER TABLE `ujn_model` ADD COLUMN `attribute_alias` varchar(255) NOT NULL DEFAULT '' COMMENT '属性别名定义' AFTER `attribute_list`;
+ALTER TABLE `ujn_category` CHANGE COLUMN `model` `model` varchar(100) NOT NULL DEFAULT '' COMMENT '列表绑定模型';
+UPDATE `ujn_attribute` SET `extra` = '[DOCUMENT_POSITION]' WHERE `id`=10;
+ALTER TABLE `ujn_hooks` ADD COLUMN `status` tinyint(1) unsigned NOT NULL DEFAULT '1';
+ALTER TABLE `ujn_category` ADD COLUMN `model_sub` varchar(100) NOT NULL DEFAULT '' COMMENT '子文档绑定模型' AFTER `model`;
+UPDATE `ujn_category` SET `model` = '2,3', `model_sub` = '2' WHERE `id` = 1;
+UPDATE `ujn_category` SET `model` = '2,3', `model_sub` = '2', `icon` = 0 WHERE `id` = 2;
+ALTER TABLE `ujn_file` ADD COLUMN `url` varchar(255) NOT NULL DEFAULT '' COMMENT '远程地址' AFTER `location`;
+ALTER TABLE `ujn_menu` ADD COLUMN `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态' AFTER `is_dev`;
+ALTER TABLE `ujn_menu` ADD INDEX `status` (`status`);
+UPDATE `ujn_menu` SET `status` = '1' ;
+UPDATE `ujn_menu` SET `url` = 'article/index' WHERE `id` =2;
+INSERT INTO `ujn_menu` (`title`,`pid`,`sort`,`url`,`hide`,`tip`,`group`,`is_dev`,`status`) VALUES ('审核列表', '3', '0', 'Article/examine', '1', '', '', '0','1');
+UPDATE `ujn_model` SET `list_grid` = 'id:编号\r\ntitle:标题:[EDIT]\r\ntype:类型\r\nupdate_time:最后更新\r\nstatus:状态\r\nview:浏览\r\nid:操作:[EDIT]|编辑,[DELETE]|删除' WHERE `id` = 1;
+UPDATE `ujn_model` set `list_grid` = '' WHERE `id` > 2;
